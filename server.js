c18f1e512d970b6bf1c71670f2d35434b8c13ba5
Sanil Khurana <sanilkhurana7@gmail.com>
2020-07-16 23:43:31 +0530

    Basic project setup

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+require('dotenv').config()
 
 mongoose.connect('mongodb://localhost/phonebook', { useNewUrlParser: true }).then(() => console.log("Database initialized."));
-
 
 const userRouter = require('./users/routes');
 const contactRouter = require('./contacts/routes');
 
 const app = express();
+
+// Setting up session and session store
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 4,
+    },
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
